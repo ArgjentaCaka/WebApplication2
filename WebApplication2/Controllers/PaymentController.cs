75c8ff7028ecfbb390afeca57ede16a418754096
ElzaGit <ek55329@ubt-uni.net>
2023-01-23 18:53:36 +0100

    commit

---
+++ WebApplication2/Controllers/PaymentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private WebEntities db = new WebEntities();
+
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View(db.payments.ToList());
+        }
+
+        // GET: Payment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            payment payment = db.payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Payment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,course,financialcode,billing,payments")] payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            payment payment = db.payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,course,financialcode,billing,payments")] payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(payment);
+        }
+
+        // GET: Payment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            payment payment = db.payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            payment payment = db.payments.Find(id);
+            db.payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
