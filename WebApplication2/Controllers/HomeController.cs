1d0c0640b1b9df60a71162ca536845fcc0e45067
38346 <38346@DESKTOP-QK8141T>
2023-01-26 17:26:56 +0100

    Commit

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -3,26 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        DBuserSignupLoginEntities db = new DBuserSignupLoginEntities();
+        // GET: Home
         public ActionResult Index()
         {
+
+            return View(db.TBLUserInfoes.ToList());
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(TBLUserInfo tBLUserInfo)
+        {
+            if (db.TBLUserInfoes.Any(x=>x.UsernameUs == tBLUserInfo.UsernameUs))
+            {
+                ViewBag.Notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.TBLUserInfoes.Add(tBLUserInfo);
+                db.SaveChanges();
+
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameUsSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index","Home");
+            }
+        } 
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        [HttpPost]
+        [ValidateAntiForgeryToken]
 
-            return View();
-        }
-
-        public ActionResult Contact()
+        public ActionResult Login(TBLUserInfo tBLUserInfo)
         {
-            ViewBag.Message = "Your contact page.";
+            var checkLogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tBLUserInfo.UsernameUs) && x.PasswordUs.Equals(tBLUserInfo.PasswordUs)).FirstOrDefault();
+            if(checkLogin != null)
+            {
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameUsSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ViewBag.Notification = "Wrong Username or password";
+            }
 
             return View();
         }
